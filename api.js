4a56371a37b139ee2127b7c9b6b3b948fef370e9
swasti12349 <swastiranjan64@gmail.com>
2022-01-26 00:19:18 +0530

    initial commit

---
+++ api.js
@@ -0,0 +1,52 @@
+const exp = require("express");
+const mong = require("mongoose");
+const body = require("body-parser");
+
+const app = exp();
+
+app.use(body.urlencoded({ extended: true }));
+
+mong.connect("mongodb://localhost:27017/apidb");
+
+const schema = {
+  name: String,
+  email: String,
+  password: String,
+};
+
+const mdel = mong.model("User", schema);
+
+app.get("/users", (req, res) => {
+  mdel.find((err, found) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(found);
+    }
+  });
+});
+
+app.post("/users",  (req, res)=>{
+    
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const m = new mdel({
+        name: name,
+        email: email,
+        password: password
+    })
+    m.save();
+    res.send("Registered");
+})
+
+
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(3000, () => {
+  console.log("Server started");
+});
