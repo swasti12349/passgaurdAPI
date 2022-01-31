e5b04854e8055da3df43694d17930b7dae71cdfa
swasti12349 <swastiranjan64@gmail.com>
2022-01-31 19:45:13 +0530

    some changes

--- api.js
+++ api.js
@@ -41,7 +41,7 @@ app.get("/users", (req, res) => {
 });
 
 // get data of a user
-app.get("/usersdata", (req, res) => {
+app.post("/usersdata", (req, res) => {
   const email = req.body.email;
   const emailstr = "A" + email + "s";
   const mdels = users.model(emailstr, dataschema);
